16b2465150bd017192bbd1690495b2cb49d13a39
Gain Jin <gainjin0187@gmail.com>
2026-06-22 12:34:34 +0800

    Update metric-method.js

--- js/metric-method.js
+++ js/metric-method.js
@@ -86,7 +86,7 @@ function formatMethodName(method) {
         'Red light camera': 'Red Light Camera',
         'Police issued': 'Police Issued',
         'Manual Action': 'Manual Action',
-        'Average speed camera': 'Avg Speed Camera',
+        'Avg speed camera': 'Avg Speed Camera',
         'Camera': 'Camera'
     };
     return names[method] || method;
